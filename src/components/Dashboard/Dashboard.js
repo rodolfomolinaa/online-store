444202240897bb63b2ff5ac38ea400dbc331abfe
Rodolfo <rodolfomolinasv@gmail.com>
2021-01-28 21:52:48 -0600

    Add Dashboard, About and Products components

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,9 @@
+function Dashboard() {
+    return (
+        <div>
+            <h1>My Dashboard component</h1>
+        </div>
+    )
+}
+
+export default Dashboard
---
+++ src/components/Products/Products.js
@@ -0,0 +1,11 @@
+function Products() {
+    return (
+        <div>
+            <h1>
+                Product component
+            </h1>
+        </div>
+    )
+}
+
+export default Products
