8158fd4ad25e5c3e1aa0a561f8c3ef73005982c1
Rodolfo <rodolfomolinasv@gmail.com>
2021-02-01 18:58:47 -0600

    Add Home and ProductDetails component in App

--- src/App.js
+++ src/App.js
@@ -2,21 +2,20 @@ import './App.css';
 import Layout from './containers/Layout/Layout';
 import { Route, Switch } from 'react-router-dom';
 
-import Dashboard from './components/Dashboard/Dashboard';
-import Products from './components/Products/Products';
+import Home from './containers/Home/Home';
+import ProductDetails from './containers/ProductDetail/ProductDetail';
 import About from './components/About/About';
-
 
 function App() {
   return (
     <Layout>
       <Switch>
-        <Route exact path="/" component={Dashboard} />
-        <Route exact path="/products" component={Products} />
+        {/* <Route exact path="/products" component={Products} /> */}
+        <Route exact path="/product/:productId" component={ProductDetails} />
         <Route exact path="/about" component={About} />
+        <Route exact path="/" component={Home} />
       </Switch>
     </Layout>
-
   );
 }
 
