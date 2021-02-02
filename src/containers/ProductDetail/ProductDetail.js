cfcad5fae6bc870ffccf5b73977c308cf4401df5
Rodolfo <rodolfomolinasv@gmail.com>
2021-02-01 18:58:02 -0600

    Create ProductDetail component

---
+++ src/containers/ProductDetail/ProductDetail.js
@@ -0,0 +1,98 @@
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom';
+
+function ProductDetail() {
+    const [product, setProductState] = useState({});
+    const params = useParams();
+    const productDB = [
+        {
+            id: 1,
+            img: "https://images-na.ssl-images-amazon.com/images/I/716hxkzc4SL._SY445_.jpg",
+            details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!",
+            title: "Lorem ipsum dolor sit amet",
+            price: "$0.05"
+        },
+        {
+            id: 2,
+            img: "https://images-na.ssl-images-amazon.com/images/I/71RJjEn8oCL._SX342_.jpg",
+            details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!",
+            title: "Lorem ipsum dolor sit amet",
+            price: "$50"
+        },
+        {
+            id: 3,
+            img: "https://images-na.ssl-images-amazon.com/images/I/51P7ctxJSvL._AC_SL1000_.jpg",
+            details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!",
+            title: "Lorem ipsum dolor sit amet",
+            price: "$11"
+        },
+        {
+            id: 4,
+            img: "https://images-na.ssl-images-amazon.com/images/I/81WMUxNTRYL._SX342_.jpg",
+            details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!",
+            title: "Lorem ipsum dolor sit amet",
+            price: "$110"
+        },
+        {
+            id: 5,
+            img: "https://images-na.ssl-images-amazon.com/images/I/81-JwILO-FL._AC_SL1500_.jpg",
+            details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!",
+            title: "Lorem ipsum dolor sit amet",
+            price: "$50"
+        },
+        {
+            id: 6,
+            img: "https://images-na.ssl-images-amazon.com/images/I/81-JwILO-FL._AC_SL1500_.jpg",
+            details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!",
+            title: "Lorem ipsum dolor sit amet",
+            price: "$50"
+        },
+        {
+            id: 7,
+            img: "https://images-na.ssl-images-amazon.com/images/I/81-JwILO-FL._AC_SL1500_.jpg",
+            details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!",
+            title: "Lorem ipsum dolor sit amet",
+            price: "$50"
+        },
+        {
+            id: 8,
+            img: "https://images-na.ssl-images-amazon.com/images/I/81-JwILO-FL._AC_SL1500_.jpg",
+            details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!",
+            title: "Lorem ipsum dolor sit amet",
+            price: "$50"
+        },
+        {
+            id: 9,
+            img: "https://images-na.ssl-images-amazon.com/images/I/81-JwILO-FL._AC_SL1500_.jpg",
+            details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!",
+            title: "Lorem ipsum dolor sit amet",
+            price: "$50"
+        },
+    ];
+
+    useEffect(() => {
+        /* ir al web service.. y obtener detalles del producto seleccionado... */
+        const product = (productDB.filter(product => product.id === parseInt(params.productId)))[0] || {};
+        setProductState({ ...product });
+    }, []);
+
+    let objectInfo = <div style={{ textAlign: "center" }}><h1>Product not found...</h1></div>;
+    if (Object.entries(product).length !== 0) {
+        objectInfo = (
+            <div style={{ textAlign: "center" }}>
+                <h1>{product.title}</h1>
+                <img src={product.img} alt="product" />
+                <h2>{product.details}</h2>
+            </div>
+        );
+    }
+
+    return (
+        <>
+            <h1>Product detail</h1>
+            {objectInfo}
+        </>
+    )
+}
+
+export default ProductDetail
