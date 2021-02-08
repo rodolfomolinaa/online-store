4278b8e5f06afa53c1eb0f63f090f3c88aca61bc
Rodolfo <rodolfomolinasv@gmail.com>
2021-02-08 11:50:51 -0600

    Remove hardcoded data from ProductDetail

--- src/containers/ProductDetail/ProductDetail.js
+++ src/containers/ProductDetail/ProductDetail.js
@@ -1,96 +1,42 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 function ProductDetail() {
-    const [product, setProductState] = useState({});
+    const [product, setProduct] = useState({});
     const params = useParams();
-    const productDB = [
-        {
-            id: 1,
-            img: "https://images-na.ssl-images-amazon.com/images/I/716hxkzc4SL._SY445_.jpg",
-            details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!",
-            title: "Lorem ipsum dolor sit amet",
-            price: "$0.05"
-        },
-        {
-            id: 2,
-            img: "https://images-na.ssl-images-amazon.com/images/I/71RJjEn8oCL._SX342_.jpg",
-            details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!",
-            title: "Lorem ipsum dolor sit amet",
-            price: "$50"
-        },
-        {
-            id: 3,
-            img: "https://images-na.ssl-images-amazon.com/images/I/51P7ctxJSvL._AC_SL1000_.jpg",
-            details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!",
-            title: "Lorem ipsum dolor sit amet",
-            price: "$11"
-        },
-        {
-            id: 4,
-            img: "https://images-na.ssl-images-amazon.com/images/I/81WMUxNTRYL._SX342_.jpg",
-            details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!",
-            title: "Lorem ipsum dolor sit amet",
-            price: "$110"
-        },
-        {
-            id: 5,
-            img: "https://images-na.ssl-images-amazon.com/images/I/81-JwILO-FL._AC_SL1500_.jpg",
-            details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!",
-            title: "Lorem ipsum dolor sit amet",
-            price: "$50"
-        },
-        {
-            id: 6,
-            img: "https://images-na.ssl-images-amazon.com/images/I/81-JwILO-FL._AC_SL1500_.jpg",
-            details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!",
-            title: "Lorem ipsum dolor sit amet",
-            price: "$50"
-        },
-        {
-            id: 7,
-            img: "https://images-na.ssl-images-amazon.com/images/I/81-JwILO-FL._AC_SL1500_.jpg",
-            details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!",
-            title: "Lorem ipsum dolor sit amet",
-            price: "$50"
-        },
-        {
-            id: 8,
-            img: "https://images-na.ssl-images-amazon.com/images/I/81-JwILO-FL._AC_SL1500_.jpg",
-            details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!",
-            title: "Lorem ipsum dolor sit amet",
-            price: "$50"
-        },
-        {
-            id: 9,
-            img: "https://images-na.ssl-images-amazon.com/images/I/81-JwILO-FL._AC_SL1500_.jpg",
-            details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!",
-            title: "Lorem ipsum dolor sit amet",
-            price: "$50"
-        },
-    ];
 
     useEffect(() => {
         /* ir al web service.. y obtener detalles del producto seleccionado... */
-        const product = (productDB.filter(product => product.id === parseInt(params.productId)))[0] || {};
-        setProductState({ ...product });
+        async function getProduct() {
+            await axios.get(`https://pokeapi.co/api/v2/pokemon/${parseInt(params.productId)}`)
+                .then(res => {
+                    let productData = {
+                        id: res.data.id,
+                        name: res.data.name,
+                        img: res.data.sprites.front_default,
+                        price: Math.floor(Math.random() * Math.floor(10) + 1),
+                        details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!",
+                    };
+                    setProduct(productData);
+                })
+        }
+        getProduct();
     }, []);
-
-    let objectInfo = <div style={{ textAlign: "center" }}><h1>Product not found...</h1></div>;
-    if (Object.entries(product).length !== 0) {
-        objectInfo = (
-            <div style={{ textAlign: "center" }}>
-                <h1>{product.title}</h1>
-                <img src={product.img} alt="product" />
-                <h2>{product.details}</h2>
-            </div>
-        );
-    }
 
     return (
         <>
-            <h1>Product detail</h1>
-            {objectInfo}
+            {
+                product ? (
+                    <div style={{ textAlign: "center" }}>
+                        <h1>{product.title}</h1>
+                        <img src={product.img} alt="product" style={{ height: "300px", width: "300px" }} />
+                        <h2>{product.details}</h2>
+                    </div>
+                ) : (
+                        <div style={{ textAlign: "center" }}><h1>Product not found...</h1></div>
+                    )
+            }
         </>
     )
 }
