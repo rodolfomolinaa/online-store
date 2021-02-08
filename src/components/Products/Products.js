a70188ec37ad1040fa8c132ce774f3a9c8c96202
Rodolfo <rodolfomolinasv@gmail.com>
2021-02-08 16:22:54 -0600

    Add handleShoppingCart method to set localStorage items

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,16 +1,43 @@
 import Product from './Product/Product';
 import { Row, CardGroup } from 'react-bootstrap';
+import useLocalStorage from '../../hooks/useLocalStorage';
+
 
 function Products({ productsList, onChangeProduct }) {
-    const handleProductChange = (product) => {
-        const products = productsList.map(p => {
+    // const handleProductChange = (product) => {
+    //     const products = productsList.map(p => {
+    //         if (p.id === product.id) {
+    //             return product
+    //         }
+    //         return p;
+    //     })
+    //     onChangeProduct(products);
+    // }
+    const [cart, setCart] = useLocalStorage('cart', []);
+
+    const handleShoppingCart = (product) => {
+        const myCart = JSON.parse(localStorage.getItem('cart'));
+        if (myCart !== null) {
+            console.log('tiene algo');
+            console.log('myCart', myCart);
+        } else {
+            console.log('esta vacio');
+            console.log('myCart', myCart);
+        }
+
+        cart.find(p => {
             if (p.id === product.id) {
-                return product
+                p.quantity += product.quantity
+                return console.log('ya existe');
+            } else {
+                return console.log('es nuevo')
             }
-            return p;
         })
-        onChangeProduct(products);
+        setCart(prev => [...prev, product]);
+        console.log('add product to shopping car', product);
+
     }
+
     return (
         <Row className="mt-5">
             <CardGroup>
@@ -18,7 +45,8 @@ function Products({ productsList, onChangeProduct }) {
                     <Product
                         key={product.id}
                         product={product}
-                        onChangeProduct={handleProductChange}
+                        // onChangeProduct={handleProductChange}
+                        onAddToCart={handleShoppingCart}
                     />
                 ))}
             </CardGroup>
