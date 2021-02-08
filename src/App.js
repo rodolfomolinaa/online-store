78c7ea09a4ec5e3823135f6453134dd634427bfd
Rodolfo <rodolfomolinasv@gmail.com>
2021-02-08 14:36:37 -0600

    Add shopping-cart route in App

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import { Route, Switch } from 'react-router-dom';
 import Home from './containers/Home/Home';
 import ProductDetails from './containers/ProductDetail/ProductDetail';
 import About from './components/About/About';
+import ShoppingCart from './components/ShoppingCart/ShoppingCart';
 
 function App() {
   return (
     <Layout>
       <Switch>
-        {/* <Route exact path="/products" component={Products} /> */}
         <Route exact path="/product/:productId" component={ProductDetails} />
+        <Route exact path="/shopping-cart" component={ShoppingCart} />
         <Route exact path="/about" component={About} />
         <Route exact path="/" component={Home} />
       </Switch>
