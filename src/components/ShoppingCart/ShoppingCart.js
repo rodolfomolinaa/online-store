6b901f929c6c7bc7577d43b04727186e18100b10
Rodolfo <rodolfomolinasv@gmail.com>
2021-02-08 16:24:30 -0600

    Add getProductsCart from localStorage

--- src/components/ShoppingCart/ShoppingCart.js
+++ src/components/ShoppingCart/ShoppingCart.js
@@ -2,12 +2,51 @@ import React from 'react'
 import Cart from './Cart/Cart';
 import Checkout from './Checkout/Checkout';
 import { Row, Col } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
 
 function ShoppingCart() {
+    const [myShoppingCart, setMyCart] = useState([]);
+    const [isEmpty, setIsEmpty] = useState(true);
+
+    const getProductsCart = () => {
+        let myCart = JSON.parse(localStorage.getItem('cart'));
+        if (myCart !== null) {
+            return myCart;
+        } else {
+            setIsEmpty(true);
+            console.log('no tienes items en tu carrito');
+        }
+    }
+
+    const handlePrice = () => {
+        console.log('change price');
+    }
+
+    useEffect(() => {
+        const getDataFromCart = async () => {
+            setIsEmpty(true);
+            let itemsCart = await getProductsCart();
+            if (itemsCart !== null) {
+                setMyCart(itemsCart);
+            }
+            setIsEmpty(false);
+        }
+        getDataFromCart();
+    }, [])
+
     return (
         <Row>
             <Col xl={8}>
-                <Cart />
+                {
+                    isEmpty ? (<div>No tienes productos en tu carrito</div>) :
+                        myShoppingCart.map((product, id) => (
+                            <Cart
+                                key={product.id}
+                                product={product}
+                                onChangeQuantity={handlePrice}
+                            />
+                        ))
+                }
             </Col>
             <Col xl={4}>
                 <Checkout />
