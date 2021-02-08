1669e578ffcfbf45ac21d8c93c8cf006001cdcfb
Rodolfo <rodolfomolinasv@gmail.com>
2021-02-08 14:30:41 -0600

    Remove fetchData from home and add getPokemons from Pokemons requests file

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,61 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import Products from '../../components/Products/Products';
 import { Row, Col } from 'react-bootstrap';
-import axios from 'axios';
+import { getPokemons } from '../../requests/Pokemon';
 
 function Home() {
-    const [offset, setOffset] = useState(0);
-    const [limit, setLimit] = useState(12);
     const [pokemonList, setPokemonList] = useState([]);
     const [loading, setLoading] = useState(false);
-    // const [error, setError] = useState();
-
-
-    const fetchDataCall = async () => {
-        try {
-
-            let { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`);
-            let pokemonsEdnpoints = data.results.map(pokemon => pokemon.url);
-            let pokemonPromises = pokemonsEdnpoints.map(endpoint => axios.get(endpoint));
-            const pokemonList = await Promise.all(pokemonPromises)
-                .then(async function (pokemonResponse) {
-                    console.log('pokemon response', pokemonResponse);
-                    let pokemonData = [];
-                    for (const pokemon of pokemonResponse) {
-                        pokemonData.push({
-                            id: pokemon.data.id,
-                            name: pokemon.data.name,
-                            img: pokemon.data.sprites.front_default,
-                            price: Math.floor(Math.random() * Math.floor(10) + 1),
-                            detail: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!"
-                        })
-                    }
-                    return pokemonData;
-                })
-
-            return pokemonList;
-        } catch (err) {
-            console.log('err', err);
-        }
-    };
-
-    const onChangeProduct = (products) => {
-        console.log('change product', products)
-        setPokemonList(products);
-    }
-
-    // const onChangeProductQuantity = () => {
-    //     console.log(quantity)
-    // }
 
     useEffect(() => {
         const fetchData = async () => {
             setLoading(true);
-            let response = await fetchDataCall();
+            let response = await getPokemons();
             setPokemonList(response);
             setLoading(false);
         }
         fetchData();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     return (
@@ -64,7 +24,10 @@ function Home() {
             <Col>
                 {!loading ? (
                     // <Products productsList={pokemonList} onChangeProduct={onChangeProduct} onChangeProductQuantity={onChangeProductQuantity} />
-                    <Products productsList={pokemonList} onChangeProduct={onChangeProduct} />
+                    <Products
+                        productsList={pokemonList}
+                    // onChangeProduct={onChangeProduct}
+                    />
                 ) : (
                         <div>Loading...</div>
                     )}
