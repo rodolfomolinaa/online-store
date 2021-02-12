d49767319cc1c3f1ae5b476a3a857bc472f4ee4a
Rodolfo <rodolfomolinasv@gmail.com>
2021-02-12 16:37:07 -0600

    Add onChangeQuantity and onRemoveItem functions

--- src/components/ShoppingCart/ShoppingCart.js
+++ src/components/ShoppingCart/ShoppingCart.js
@@ -1,56 +1,81 @@
 import React from 'react'
-import Cart from './Cart/Cart';
+import ItemCart from './ItemCart/ItemCart';
 import Checkout from './Checkout/Checkout';
 import { Row, Col } from 'react-bootstrap';
 import { useEffect, useState } from 'react';
 
 function ShoppingCart() {
-    const [myShoppingCart, setMyCart] = useState([]);
+    const [myShoppingCart, setMyShoppingCart] = useState([]);
     const [isEmpty, setIsEmpty] = useState(true);
 
-    const getProductsCart = () => {
-        let myCart = JSON.parse(localStorage.getItem('cart'));
-        if (myCart !== null) {
-            return myCart;
+    const handlePrice = (action, productId) => {
+        const productsUpdated = myShoppingCart.map(item => {
+            if (item.id === productId) {
+                const quantity = action === 'decrease' ? item.quantity - 1 : item.quantity + 1
+                return {
+                    ...item,
+                    quantity: quantity,
+                    price: item.basePrice * quantity
+                }
+            }
+            return item;
+        })
+        setMyShoppingCart(productsUpdated);
+        localStorage.setItem('cart', JSON.stringify(productsUpdated));
+    }
+
+    const removeItem = (productId) => {
+        console.log('remove', productId);
+        const myNewCart = myShoppingCart.filter(item => item.id !== productId);
+        console.log('myNewCart', myNewCart);
+        if (myNewCart.length > 0) {
+            setIsEmpty(false);
+            setMyShoppingCart(myNewCart);
+            localStorage.setItem('cart', JSON.stringify(myNewCart));
         } else {
-            setIsEmpty(true);
-            console.log('no tienes items en tu carrito');
+            setIsEmpty(true)
+            localStorage.setItem('cart', JSON.stringify(myNewCart));
         }
     }
 
-    const handlePrice = () => {
-        console.log('change price');
-    }
-
     useEffect(() => {
-        const getDataFromCart = async () => {
-            setIsEmpty(true);
-            let itemsCart = await getProductsCart();
-            if (itemsCart !== null) {
-                setMyCart(itemsCart);
+        const getDataFromCart = () => {
+            const myCart = JSON.parse(localStorage.getItem('cart'));
+            if (myCart !== null && myCart.length > 0) {
+                setIsEmpty(false);
+                setMyShoppingCart(myCart);
+            } else {
+                setIsEmpty(true);
             }
-            setIsEmpty(false);
         }
         getDataFromCart();
     }, [])
 
     return (
         <Row>
-            <Col xl={8}>
-                {
-                    isEmpty ? (<div>No tienes productos en tu carrito</div>) :
-                        myShoppingCart.map((product, id) => (
-                            <Cart
-                                key={product.id}
-                                product={product}
-                                onChangeQuantity={handlePrice}
-                            />
-                        ))
-                }
-            </Col>
-            <Col xl={4}>
-                <Checkout />
-            </Col>
+            {
+                isEmpty ? (
+                    <Col xl={12}>
+                        <div>No tienes productos en tu carrito</div>
+                    </Col>
+                ) : (
+                        <>
+                            <Col xl={8}>
+                                {myShoppingCart.map((product, id) => (
+                                    <ItemCart
+                                        key={product.id}
+                                        product={product}
+                                        onChangeQuantity={handlePrice}
+                                        onRemoveItem={removeItem}
+                                    />
+                                ))}
+                            </Col>
+                            <Col xl={4}>
+                                <Checkout />
+                            </Col>
+                        </>
+                    )
+            }
         </Row>
     )
 }
