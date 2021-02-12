ab92c2dd324a53974baba4be262a4dd8c3a11b53
Rodolfo <rodolfomolinasv@gmail.com>
2021-02-12 16:37:36 -0600

    Add products length and sum in checkout component

--- src/components/ShoppingCart/Checkout/Checkout.js
+++ src/components/ShoppingCart/Checkout/Checkout.js
@@ -1,19 +1,29 @@
 function Checkout() {
+    const products = JSON.parse(localStorage.getItem('cart'));
+    // let total = products.reduce((a, b) => a.price + b.price);
+    let sum = 0;
+    products.map(item => sum += item.price)
+    console.log('sum', sum)
+
     return (
         <div className="mb-3">
             <div className="pt-4">
-                <h5 className="mb-3">The total amount of</h5>
+                <h5 className="mb-3">Products({products.length})</h5>
 
                 <ul className="list-group list-group-flush">
                     <li className="list-group-item d-flex justify-content-between align-items-center border-0 px-0 pb-0">
-                        Temporary amount
+                        Total before taxes
                                   <span>$25.98</span>
                     </li>
                     <li className="list-group-item d-flex justify-content-between align-items-center border-0 px-0 mb-3">
                         <div>
                             <strong>The total amount of</strong>
                         </div>
-                        <span><strong>$53.98</strong></span>
+                        <span>
+                            <strong>
+                                $53.98
+                            </strong>
+                        </span>
                     </li>
                 </ul>
                 <button type="button" className="btn btn-primary btn-block">Checkout</button>
