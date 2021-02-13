308e5c9ed72025c47e4aa49d78a5c5a35aec45fa
Rodolfo <rodolfomolinasv@gmail.com>
2021-02-13 12:24:17 -0600

    Fix total products quantity

--- src/components/ShoppingCart/Checkout/Checkout.js
+++ src/components/ShoppingCart/Checkout/Checkout.js
@@ -1,27 +1,22 @@
 function Checkout() {
     const products = JSON.parse(localStorage.getItem('cart'));
-    // let total = products.reduce((a, b) => a.price + b.price);
-    let sum = 0;
-    products.map(item => sum += item.price)
-    console.log('sum', sum)
+    let totalProducts = 0;
+    let totalAmout = 0;
+    products.map(item => totalProducts += item.quantity);
+    products.map(item => totalAmout += item.price)
 
     return (
         <div className="mb-3">
             <div className="pt-4">
-                <h5 className="mb-3">Products({products.length})</h5>
-
+                <h5 className="mb-3">Products({totalProducts})</h5>
                 <ul className="list-group list-group-flush">
-                    <li className="list-group-item d-flex justify-content-between align-items-center border-0 px-0 pb-0">
-                        Total before taxes
-                                  <span>$25.98</span>
-                    </li>
                     <li className="list-group-item d-flex justify-content-between align-items-center border-0 px-0 mb-3">
                         <div>
                             <strong>The total amount of</strong>
                         </div>
                         <span>
                             <strong>
-                                $53.98
+                                ${totalAmout.toFixed(2)}
                             </strong>
                         </span>
                     </li>
