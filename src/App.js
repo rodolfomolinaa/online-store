5d6d203effd7df015c75de4c5f06a36276ac2b79
Rodolfo <rodolfomolinasv@gmail.com>
2021-02-15 21:19:03 -0600

    Change routes to new directory

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import './App.css';
 import Layout from './containers/Layout/Layout';
 import { Route, Switch } from 'react-router-dom';
 
-import Home from './containers/Home/Home';
-import ProductDetails from './containers/ProductDetail/ProductDetail';
+import Products from './containers/Products/Products';
+import ProductDetail from './containers/Products/ProductDetail/ProductDetail'
 import About from './components/About/About';
-import ShoppingCart from './components/ShoppingCart/ShoppingCart';
+import ShoppingCart from './containers/ShoppingCart/ShoppingCart';
 
 function App() {
   return (
     <Layout>
       <Switch>
-        <Route exact path="/product/:productId" component={ProductDetails} />
+        <Route exact path="/product/:productId" component={ProductDetail} />
         <Route exact path="/shopping-cart" component={ShoppingCart} />
         <Route exact path="/about" component={About} />
-        <Route exact path="/" component={Home} />
+        <Route exact path="/" component={Products} />
       </Switch>
     </Layout>
   );
