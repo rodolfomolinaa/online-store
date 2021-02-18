2952abc6079f3cd83b900e9f2410816c634290ad
Rodolfo <rodolfomolinasv@gmail.com>
2021-02-17 23:06:01 -0600

    Remove basePrice, newPrice states and useEffect and add handleQuantity function

--- src/components/Products/Product/Product.js
+++ src/components/Products/Product/Product.js
@@ -1,65 +1,74 @@
-import { Link } from 'react-router-dom'
-import { Row, Col, Card, Button, Form, Spinner } from 'react-bootstrap';
-import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Row, Col, Card, Button, Form } from 'react-bootstrap';
+import { useState } from 'react';
 
-function Product({ product, shoppingCart }) {
-    const [quantity, setQuantity] = useState(1);
-    const [basePrice] = useState(product.price);
-    const [newPrice, setNewPrice] = useState();
+function Product({ index, product, shoppingCart }) {
+    const [quantity, setQuantity] = useState(product.quantity);
+    const [total, setTotal] = useState(product.price);
 
     const addToShoppingCart = () => {
         const newProduct = {
             ...product,
-            price: newPrice,
-            basePrice: basePrice,
             quantity: quantity,
-        }
-        shoppingCart(newProduct);
-    }
+        };
+        shoppingCart(newProduct, index);
+    };
 
-    useEffect(() => {
-        setNewPrice(Math.round(basePrice * quantity));
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [quantity])
-
-
+    const handleQuantity = (e) => {
+        setQuantity(parseInt(e.target.value));
+        setTotal(e.target.value * product.price);
+    };
 
     return (
-        <Col xs={12} sm={8} md={6} lg={4} xl={2} className="d-flex align-items-stretch">
-            {product ? (
-                <Card className="mb-4">
-                    <Link to={`/product/${product.id}`}>
-                        <Card.Img variant="top" src={product.img} />
-                    </Link>
-                    <Card.Body>
-                        <Card.Title>{product.name}</Card.Title>
-                    </Card.Body>
-                    <Card.Footer>
-                        <Form>
-                            <Form.Group as={Row}>
-                                <Col xs={6}>
-                                    <Form.Control type="number" min="1" value={quantity} onChange={e => setQuantity(parseInt(e.target.value))} />
-                                </Col>
-                                <Form.Label column xs={6} className="d-flex justify-content-end">
-                                    ${newPrice}
-                                </Form.Label>
-                                <Col xs={12}>
-                                    <Button variant="primary" className="mt-2" block onClick={addToShoppingCart}>
-                                        Add to cart
-                                    </Button>
-                                </Col>
-                            </Form.Group>
-                        </Form>
-                    </Card.Footer>
-                </Card>
-            ) : (
-                    <div className="d-flex justify-content-center">
-                        <Spinner animation="border" size="xl" />
-                        <div>Loading...</div>
-                    </div>
-                )}
+        <Col
+            xs={12}
+            sm={8}
+            md={6}
+            lg={4}
+            xl={2}
+            className="d-flex align-items-stretch"
+        >
+            <Card className="mb-4">
+                <Link to={`/product/${product.id}`}>
+                    <Card.Img variant="top" src={product.img} />
+                </Link>
+                <Card.Body>
+                    <Card.Title>{product.name}</Card.Title>
+                </Card.Body>
+                <Card.Footer>
+                    <Form>
+                        <Form.Group as={Row}>
+                            <Col xs={6}>
+                                <Form.Control
+                                    type="number"
+                                    min="1"
+                                    value={quantity}
+                                    onChange={(e) => handleQuantity(e)}
+                                />
+                            </Col>
+                            <Form.Label
+                                column
+                                xs={6}
+                                className="d-flex justify-content-end"
+                            >
+                                ${total.toFixed(2)}
+                            </Form.Label>
+                            <Col xs={12}>
+                                <Button
+                                    variant="primary"
+                                    className="mt-2"
+                                    block
+                                    onClick={addToShoppingCart}
+                                >
+                                    Add to cart
+                                </Button>
+                            </Col>
+                        </Form.Group>
+                    </Form>
+                </Card.Footer>
+            </Card>
         </Col>
-    )
+    );
 }
 
 export default Product;
