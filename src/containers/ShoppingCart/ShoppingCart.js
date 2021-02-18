b50c6ec7b4eab8878365a287e302cf124c67a5f8
Rodolfo <rodolfomolinasv@gmail.com>
2021-02-17 23:06:32 -0600

    Add useLocalStorage hook and index in ItemCart mapping

--- src/containers/ShoppingCart/ShoppingCart.js
+++ src/containers/ShoppingCart/ShoppingCart.js
@@ -1,83 +1,65 @@
-import React from 'react'
+import React from 'react';
 import ItemCart from '../../components/ShoppingCart/ItemCart/ItemCart';
 import Checkout from '../../components/ShoppingCart/Checkout/Checkout';
 import { Row, Col } from 'react-bootstrap';
 import { useEffect, useState } from 'react';
+import useLocalStorage from '../../hooks/useLocalStorage';
 
 function ShoppingCart() {
-    const [myShoppingCart, setMyShoppingCart] = useState([]);
+    const [myShoppingCart, setMyShoopingCart] = useLocalStorage('cart');
     const [isEmpty, setIsEmpty] = useState(true);
 
-    const handlePrice = (action, productId) => {
-        const productsUpdated = myShoppingCart.map(item => {
-            if (item.id === productId) {
-                const quantity = action === 'decrease' ? item.quantity - 1 : item.quantity + 1
-                return {
-                    ...item,
-                    quantity: quantity,
-                    price: item.basePrice * quantity
-                }
-            }
-            return item;
-        })
-        setMyShoppingCart(productsUpdated);
-        localStorage.setItem('cart', JSON.stringify(productsUpdated));
-    }
+    const handlePrice = (action, index) => {
+        const updateShoppingCart = [...myShoppingCart];
+        const quantity =
+            action === 'decrease'
+                ? updateShoppingCart[index].quantity - 1
+                : updateShoppingCart[index].quantity + 1;
 
-    const removeItem = (productId) => {
-        console.log('remove', productId);
-        const myNewCart = myShoppingCart.filter(item => item.id !== productId);
-        console.log('myNewCart', myNewCart);
-        if (myNewCart.length > 0) {
-            setIsEmpty(false);
-            setMyShoppingCart(myNewCart);
-            localStorage.setItem('cart', JSON.stringify(myNewCart));
-        } else {
-            setIsEmpty(true)
-            localStorage.setItem('cart', JSON.stringify(myNewCart));
-        }
-    }
+        updateShoppingCart[index] = {
+            ...updateShoppingCart[index],
+            quantity: quantity,
+        };
+        setMyShoopingCart(updateShoppingCart);
+    };
+
+    const removeItem = (index) => {
+        const updateShoppingCart = [...myShoppingCart];
+        updateShoppingCart.splice(index, 1);
+        setMyShoopingCart(updateShoppingCart);
+        myShoppingCart.length > 0 ? setIsEmpty(false) : setIsEmpty(false);
+    };
 
     useEffect(() => {
-        const getDataFromCart = () => {
-            const myCart = JSON.parse(localStorage.getItem('cart'));
-            if (myCart !== null && myCart.length > 0) {
-                setIsEmpty(false);
-                setMyShoppingCart(myCart);
-            } else {
-                setIsEmpty(true);
-            }
-        }
-        getDataFromCart();
-    }, [])
+        myShoppingCart.length > 0 ? setIsEmpty(false) : setIsEmpty(true);
+    }, [myShoppingCart]);
 
     return (
         <Row>
-            {
-                isEmpty ? (
-                    <Col xl={12}>
-                        <div>No tienes productos en tu carrito</div>
+            {isEmpty ? (
+                <Col xl={12}>
+                    <div>No tienes productos en tu carrito</div>
+                </Col>
+            ) : (
+                <>
+                    <Col xl={8}>
+                        {myShoppingCart.map((product, index) => (
+                            <ItemCart
+                                key={product.id}
+                                index={index}
+                                product={product}
+                                onChangeQuantity={handlePrice}
+                                onRemoveItem={removeItem}
+                            />
+                        ))}
                     </Col>
-                ) : (
-                        <>
-                            <Col xl={8}>
-                                {myShoppingCart.map((product, id) => (
-                                    <ItemCart
-                                        key={product.id}
-                                        product={product}
-                                        onChangeQuantity={handlePrice}
-                                        onRemoveItem={removeItem}
-                                    />
-                                ))}
-                            </Col>
-                            <Col xl={4}>
-                                <Checkout />
-                            </Col>
-                        </>
-                    )
-            }
+                    <Col xl={4}>
+                        <Checkout />
+                    </Col>
+                </>
+            )}
         </Row>
-    )
+    );
 }
 
-export default ShoppingCart
+export default ShoppingCart;
