a273f3c8ac03f32545583db45c701b8aac5f9548
Rodolfo <rodolfomolinasv@gmail.com>
2021-03-09 14:18:05 -0600

    Add Product requests file

---
+++ src/requests/Product.js
@@ -0,0 +1,38 @@
+import axios from '../axios/axios';
+
+export const getProducts = async () => {
+    try {
+        let { data } = await axios.get('/products');
+        data = data.products.map((product) => ({
+            id: product.id,
+            title: product.title,
+            price: product.price,
+            stock: product.stock,
+            quantity: 1,
+            detail: product.detail,
+            img: product.imageUrl,
+        }));
+        return data;
+    } catch (error) {
+        console.log('error', error);
+    }
+};
+
+export const getProduct = async (productId) => {
+    try {
+        let response = await axios.get(`/products/${productId}`);
+        console.log('response in request', response);
+        const product = {
+            id: response.data.product.id,
+            title: response.data.product.title,
+            price: response.data.product.price,
+            stock: response.data.product.stock,
+            detail: response.data.product.detail,
+            img: response.data.product.imageUrl,
+        };
+
+        return product;
+    } catch (error) {
+        console.log(error);
+    }
+};
