a024872dc6d9c59e550a0e889d941df0e4203644
Rodolfo <rodolfomolinasv@gmail.com>
2021-03-09 14:19:54 -0600

    Format code in app

--- src/App.js
+++ src/App.js
@@ -3,21 +3,25 @@ import Layout from './containers/Layout/Layout';
 import { Route, Switch } from 'react-router-dom';
 
 import Products from './containers/Products/Products';
-import ProductDetail from './containers/Products/ProductDetail/ProductDetail'
+import ProductDetail from './containers/Products/ProductDetail/ProductDetail';
 import About from './components/About/About';
 import ShoppingCart from './containers/ShoppingCart/ShoppingCart';
 
 function App() {
-  return (
-    <Layout>
-      <Switch>
-        <Route exact path="/product/:productId" component={ProductDetail} />
-        <Route exact path="/shopping-cart" component={ShoppingCart} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/" component={Products} />
-      </Switch>
-    </Layout>
-  );
+    return (
+        <Layout>
+            <Switch>
+                <Route
+                    exact
+                    path="/product/:productId"
+                    component={ProductDetail}
+                />
+                <Route exact path="/shopping-cart" component={ShoppingCart} />
+                <Route exact path="/about" component={About} />
+                <Route exact path="/" component={Products} />
+            </Switch>
+        </Layout>
+    );
 }
 
 export default App;
