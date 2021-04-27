fd40c2f2d46f3d0806e2fc820ee361fe06ee4a8f
Rodolfo <rodolfomolinasv@gmail.com>
2021-04-27 15:22:03 -0600

    Add EmptyCart component

---
+++ src/components/ShoppingCart/EmptyCart/EmptyCart.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Jumbotron, Button } from 'react-bootstrap';
+import { FaShoppingCart } from 'react-icons/fa';
+import { IconContext } from 'react-icons/lib';
+import { Nav } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+
+function EmptyCart() {
+    return (
+        <Jumbotron className="text-center mt-4">
+            <IconContext.Provider
+                value={{
+                    size: '12em',
+                    color: 'cornflowerblue',
+                }}
+            >
+                <FaShoppingCart />
+            </IconContext.Provider>
+            <h1>Your cart is empty</h1>
+            <p>Add something to make me happy</p>
+            <p>
+                <LinkContainer to="/" exact>
+                    <Nav.Link>
+                        <Button>Continue Shopping</Button>
+                    </Nav.Link>
+                </LinkContainer>
+            </p>
+        </Jumbotron>
+    );
+}
+
+export default EmptyCart;
