5f3556d006e13846769e53772c3d84455936a39f
Rodolfo <rodolfomolinasv@gmail.com>
2021-04-27 15:24:51 -0600

    Add Receipt and NotFound components

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { Route, Switch } from 'react-router-dom';
 import Products from './containers/Products/Products';
 import ProductDetail from './containers/Products/ProductDetail/ProductDetail';
 import ShoppingCart from './containers/ShoppingCart/ShoppingCart';
+import Receipt from './components/ShoppingCart/Receipt/Receipt';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
     return (
@@ -16,7 +18,9 @@ function App() {
                     component={ProductDetail}
                 />
                 <Route exact path="/shopping-cart" component={ShoppingCart} />
+                <Route exact path="/purchase/:purchaseId" component={Receipt} />
                 <Route exact path="/" component={Products} />
+                <Route parth="*" component={NotFound} />
             </Switch>
         </Layout>
     );
