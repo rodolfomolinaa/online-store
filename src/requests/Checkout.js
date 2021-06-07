d49f7aa6a6d99ad9ad3682369cf32823505ddadc
Rodolfo <rodolfomolinasv@gmail.com>
2021-06-07 13:49:33 -0600

    Add config from constants

--- src/requests/Checkout.js
+++ src/requests/Checkout.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
+import { config } from '../Constants';
 
 export const save = async (products) => {
-    const { data } = await axios.post('http://localhost:8080/purchase/', {
+    const { data } = await axios.post(`${config.url.checkoutApi}/purchase`, {
         products,
     });
     return data;
 };
 
 export const getPurchase = async (id) => {
-    let response = await axios.get(`http://localhost:8080/purchase/${id}`);
+    let response = await axios.get(`${config.url.checkoutApi}/purchase/${id}`);
     return response;
 };
