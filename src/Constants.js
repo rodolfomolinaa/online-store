698819db5735fc5c9240fd389f3d8de037f440d4
Rodolfo <rodolfomolinasv@gmail.com>
2021-06-07 13:34:01 -0600

    Add Constants with prod and dev config

---
+++ src/Constants.js
@@ -0,0 +1,16 @@
+const prod = {
+    url: {
+        productsApi: 'https://online-store-312700.wn.r.appspot.com/api',
+        checkoutApi:
+            'https://checkout-dot-online-store-312700.wn.r.appspot.com',
+    },
+};
+
+const dev = {
+    url: {
+        productsApi: 'http://localhost:5000/api',
+        checkoutApi: 'http://localhost:8080/purchase/',
+    },
+};
+
+export const config = process.env.NODE_ENV === 'development' ? dev : prod;
