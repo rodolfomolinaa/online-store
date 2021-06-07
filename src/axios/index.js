34d7c78a5138bdf994fcf3b45d5468b98095e5e5
Rodolfo <rodolfomolinasv@gmail.com>
2021-06-07 13:35:16 -0600

    Add config from constants in axios

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
+import { config } from '../Constants';
 
 const instance = axios.create({
-    baseURL: process.env.REACT_APP_DEVELOPMENT,
+    // baseURL: process.env.REACT_APP_DEVELOPMENT,
+    baseURL: config.url.productsApi,
 });
 
 export default instance;
